ddc3d1ed3e3cea7deb955a1776d8e864aeed60c0
cchiu615@gmail.com <candichiu@Candis-MacBook-Pro.local>
2021-05-28 15:03:41 +0200

    iOS Automation

--- Appiumpython/iOSAutomation/iOS_launchApp.py
+++ Appiumpython/iOSAutomation/iOS_launchApp.py
@@ -2,7 +2,7 @@ from appium import webdriver
 
 desired_caps = {}
 desired_caps['platformName'] = 'IOS'
-desired_caps['platformVersion'] = '14.4'
+desired_caps['platformVersion'] = '14.5'
 desired_caps['deviceName'] = 'iPhone 11'
 desired_caps['automationName'] = 'XCUITest'
 desired_caps['app'] = '/Users/candichiu/Documents/iOSAutomation/UICatalog.app'
